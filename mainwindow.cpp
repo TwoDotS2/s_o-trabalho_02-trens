ee0c6c37f8c73abdf7aa2a69052ea4ddc32e5792
afon-SO- <09afonso09@gmailcom>
2022-11-15 10:47:52 -0300

    testando casos

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define T1 0
+#define T2 1
+#define T3 2
+#define T4 3
+#define T5 4
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -54,7 +60,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //Inicialização das threads
     trem1->start();
-    printf("INICIANDO \n");
+    //printf("INICIANDO \n");
     trem2->start();
     trem3->start();
     trem4->start();
@@ -101,93 +107,356 @@ void MainWindow::entrar_em_regiao(int ID, int _regiao){
     switch (ID) {
 
     case 1:
-        if(_regiao == 0){
-            printf("ENTRANDO NA REGIAO 0, state: ", trem_por_regiao[ID-1]);
-            while(trem_por_regiao[ID-1] == ZONA_LIVRE){
-                printf("REGIAO 0 LIVRE PRA TREM 1 \n");
+
+        //Trem 1
+        // O Trem 1 usa as regiões: 0, 2, 1.
+
+       //Acessar região 0
+       if(_regiao == 0){
+
+           while(trem_por_regiao[T1] == ZONA_LIVRE){
+
+               //Trava o "mutex"
+               mutex.acquire(1);
+
+               if( trem_por_regiao[T2] != 3 && trem_por_regiao[T4] != 2 )
+                   trem_por_regiao[T1] = 0;
+
+               //Libera o "mutex"
+               mutex.release(1);
+           }
+
+           //Se tem acesso a região, se desloca
+           if(trem_por_regiao[T1] == 0){
+               regiao[5].acquire(1);
+           }
+       }
+
+       //Acessar região 2
+        if(_regiao == 2){
+        //printf("ENTRANDO NA REGIAO 2, state: ", trem_por_regiao[T1]);
+
+            while(trem_por_regiao[T1] == 0){
                 //Trava o "mutex"
                 mutex.acquire(1);
 
-                //Caso os trens 3, 4 não ocuparem as regiões 2 e 5, respecivamente
-                // o trem 1 pode acesser a região 0
-                if(trem_por_regiao[3] != 2 && trem_por_regiao[4] != 5){
-                    printf("TÁ LIVRE TREM 1 PRA PASSAR \n");
-                    trem_por_regiao[0] = 0;
-                 }
+                if(!(trem_por_regiao[T3] == 1 && trem_por_regiao[T4] == 5))
+                    trem_por_regiao[T1] = 2;
+
                 //Libera o "mutex"
                 mutex.release(1);
+
             }
 
-            if(trem_por_regiao[0] == 0){
-                (regiao[0]).acquire(1); //trava a regiao 0
-                printf("AGORA ANDE\n");
-                trem1->set_x(ui->label_trem1->x()+10);
+            if(trem_por_regiao[T1] == 2){
+                (regiao[2]).acquire(1);
+                trem1->set_y(ui->label_trem1->x()-10);
             }
+        }
 
-      }
+        //Acessar região 1
+        if(_regiao == 1){
+            //printf("ENTRANDO NA REGIAO 1, state: ", trem_por_regiao[T1]);
 
-        if(_regiao == 2){
-            printf("ENTRANDO NA REGIAO 2, state: ", trem_por_regiao[ID-1]);
-            while(trem_por_regiao[ID-1] == ZONA_LIVRE){
+            while(trem_por_regiao[T1] == 2){
+                //Trava o "mutex"
                 mutex.acquire(1);
 
-                printf("REGIAO 2 LIVRE PRA TREM 1 \n");
-                //Caso os trens 4, 2 não ocuparem as regiões 5, 4 respecivamente
-                // o trem 1 pode acesser a região 2
-                if(trem_por_regiao[4] != 5 && trem_por_regiao[2] != 4){
-                    printf("TÁ LIVRE TREM 1 PRA PASSAR \n");
-                    trem_por_regiao[0] = 0;
-                 }
+//                if(/*condição*/)
+                    trem_por_regiao[T1] = 1;
+
                 //Libera o "mutex"
                 mutex.release(1);
 
             }
 
-            if(trem_por_regiao[ID-1] == 2){
-                (regiao[2]).acquire(1); //trava a regiao 2
-                printf("AGORA ANDE");
-                trem1->set_y(ui->label_trem1->y()+10);
+            if(trem_por_regiao[T1] == 1){
+                regiao[1].acquire(1);
+                trem1->set_y(ui->label_trem1->x()-10);
             }
         }
 
         break;
 
     case 2:
-//
+
+//        //Trem 2
+//        // O Trem 2 usa as regiões: 4, 3, 0.
+
+//       //Acessar região 4
+//       if(_regiao == 4){
+//         //printf("ENTRANDO NA REGIAO 4, state: ", trem_por_regiao[T2]);
+
+//           while(trem_por_regiao[T2] == ZONA_LIVRE){
+
+//               //Trava o "mutex"
+//               mutex.acquire(1);
+
+//               if( trem_por_regiao[T4] != 3 && trem_por_regiao[T4] != 2 && trem_por_regiao[T5] != 6 )
+//                   trem_por_regiao[T2] = 4;
+
+//               //Libera o "mutex"
+//               mutex.release(1);
+//           }
+
+//           //Se tem acesso a região, se desloca
+//           if(trem_por_regiao[T2] == 4){
+//               regiao[4].acquire(1);
+//               trem2->set_x(ui->label_trem1->x()-10);
+//           }
+//       }
+
+//       //Acessar região 3
+//        if(_regiao == 3){
+//        //printf("ENTRANDO NA REGIAO 3, state: ", trem_por_regiao[T2]);
+
+//            while(trem_por_regiao[T2] == 4){
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                if(!(trem_por_regiao[T1] == 0 && trem_por_regiao[T4] == 2))
+//                    trem_por_regiao[T2] = 3;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+
+//            }
+
+//            if(trem_por_regiao[T2] == 3){
+//                (regiao[3]).acquire(1);
+//                trem2->set_y(ui->label_trem1->x()-10);
+//            }
+//        }
+
+//        //Acessar região 0
+//        if(_regiao == 0){
+//            //printf("ENTRANDO NA REGIAO 0, state: ", trem_por_regiao[T2]);
+
+//            while(trem_por_regiao[T2] == 3){
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+////               if(/*condição*/)
+//                    trem_por_regiao[T2] = 0;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+
+//            }
+
+//            if(trem_por_regiao[T2] == 0){
+//                regiao[0].acquire(1);
+//                trem2->set_y(ui->label_trem1->y()-10);
+//            }
+//        }
+
         break;
 
     case 3:
-//        if(regiao == 1){
 
-//        } else if(regiao == 5){
+//        //Trem 3
+//        // O Trem 3 usa as regiões: 1, 5.
 
+
+//        //Acessar região 1
+//        if(_regiao == 1){
+//            //printf("ENTRANDO NA REGIAO 1, state: ", trem_por_regiao[T3]);
+
+//            while(trem_por_regiao[T3] == ZONA_LIVRE){
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                if(!(trem_por_regiao[T1] == 2 && trem_por_regiao[T4] == 5))
+//                    trem_por_regiao[T3] = 1;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+
+//            }
+
+//            if(trem_por_regiao[T3] == 1){
+//                regiao[1].acquire(1);
+//                trem3->set_y(ui->label_trem1->x()+10);
+//            }
 //        }
+
+//        //Acessar região 5
+//       if(_regiao == 5){
+//           //printf("ENTRANDO NA REGIAO 5, state: ", trem_por_regiao[T3]);
+
+//           while(trem_por_regiao[T3] == 1){
+
+//               //Trava o "mutex"
+//               mutex.acquire(1);
+
+////               if( /**/ )
+//                   trem_por_regiao[T3] = 5;
+
+//               //Libera o "mutex"
+//               mutex.release(1);
+//           }
+
+//           //Se tem acesso a região, se desloca
+//           if(trem_por_regiao[T3] == 5){
+//               regiao[5].acquire(1);
+//               trem3->set_x(ui->label_trem1->y()+10);
+//           }
+//       }
 
         break;
 
     case 4:
-//        if(regiao == 0){
 
-//        } else if(regiao == 2){
+//        //Trem 4
+//        // O Trem 4 usa as regiões: 5, 2, 3, 6.
 
-//        } else if(regiao == 3){
+//         //Acessar região 5
+//        if(_regiao == 5){
+//            //printf("ENTRANDO NA REGIAO 5, state: ", trem_por_regiao[T4]);
 
-//        } else if(regiao == 5){
+//            while(trem_por_regiao[T4] == ZONA_LIVRE){
 
-//        } else if(regiao == 6){
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                if( trem_por_regiao[T3] != 1 )
+//                    trem_por_regiao[T4] = 5;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+//            }
+
+//            //Se tem acesso a região, se desloca
+//            if(trem_por_regiao[T4] == 5){
+//                regiao[5].acquire(1);
+//                trem4->set_y(ui->label_trem1->y()-10);
+//            }
+//        }
+
+//        //Acessar região 2
+//        if(_regiao == 2){
+//            //printf("ENTRANDO NA REGIAO 2, state: ", trem_por_regiao[T4]);
+
+//            while(trem_por_regiao[T4] == 5){
+
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                if( trem_por_regiao[T2] != 3 )
+//                    trem_por_regiao[T4] = 2;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+//            }
+
+//            if(trem_por_regiao[T4] == 2){
+//                regiao[2].acquire(1);
+//                trem4->set_x(ui->label_trem1->x()+10);
+//            }
 
 //        }
+
+//        //Acessar região 3
+//        if(_regiao == 3){
+//            //printf("ENTRANDO NA REGIAO 3, state: ", trem_por_regiao[T4]);
+
+//            while(trem_por_regiao[T4] == 2){
+
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                //if ( /*condição*/ )
+//                trem_por_regiao[T4] = 3;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+//            }
+
+//            if(trem_por_regiao[T4] == 3){
+//                regiao[3].acquire(1);
+//                trem4->set_x(ui->label_trem1->x()+10);
+//            }
+//        }
+
+//        //Acessar região 6
+//        if(_regiao == 6){
+//            //printf("ENTRANDO NA REGIAO 6, state: ", trem_por_regiao[T4]);
+
+//            while(trem_por_regiao[T4] == 3){
+
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                //if ( /*condição*/ )
+//                trem_por_regiao[T4] = 6;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+//            }
+
+//            if(trem_por_regiao[T3] == 6){
+//                regiao[6].acquire(1);
+//                trem4->set_x(ui->label_trem1->y()+10);
+//            }
+
+//        }
+
         break;
-    case 5:
-//        if(regiao == 4){
 
-//        } else if(regiao == 6){
+    case 5:
+
+//        //Trem 5
+//        // O Trem 5 usa as regiões: 6, 4.
+
+//        if(_regiao == 6){
+//            //printf("ENTRANDO NA REGIAO 6, state: ", trem_por_regiao[T5]);
+
+//            while(trem_por_regiao[T5] == ZONA_LIVRE){
+
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+//                if ( trem_por_regiao[T4] != 3 )
+//                    trem_por_regiao[T5] = 6;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+//            }
+
+//            if(trem_por_regiao[T5] == 6){
+//                regiao[6].acquire(1);
+//                trem5->set_x(ui->label_trem1->y()-10);
+//            }
 
 //        }
+
+//        if(_regiao == 4){
+//          //printf("ENTRANDO NA REGIAO 4, state: ", trem_por_regiao[T5]);
+
+//            while(trem_por_regiao[T5] == 6){
+
+//                //Trava o "mutex"
+//                mutex.acquire(1);
+
+////                if( /**/)
+//                    trem_por_regiao[T5] = 4;
+
+//                //Libera o "mutex"
+//                mutex.release(1);
+//            }
+
+//            //Se tem acesso a região, se desloca
+//            if(trem_por_regiao[T5] == 4){
+//                regiao[4].acquire(1);
+//                trem5->set_x(ui->label_trem1->x()+10);
+//            }
+//        }
+
         break;
 
     default:
         break;
+
    }
 }
 
@@ -195,7 +464,6 @@ void MainWindow::entrar_em_regiao(int ID, int _regiao){
 
 void MainWindow::sair_de_regiao(int num_regiao){
     regiao[num_regiao].release(1);
-//    trem_por_regiao[...] = ZONA_LIVRE;
     return;
 }
 
--- trem.cpp
+++ trem.cpp
@@ -34,12 +34,18 @@ void Trem::run(){
         switch(ID){
             case 1:
                 //Trem 1
-                // O Trem 1 usa as regiões: 0, 2, 5, 1.
+                // O Trem 1 usa as regiões: 0, 2, 1.
 
                 //Região 0
                 if (x == 420 && y == 110){
                     //Entrar na região 0
                     emit entrar_em_regiao(ID, 0);
+
+                } else if(x < 440 && y == 110){
+                    x += deslocamento;
+
+                } else if(x == 440 && y < 210){
+                    y += deslocamento;
 
                 } else if (x == 440 && y == 230){
                     //Sair da região 0
@@ -47,35 +53,30 @@ void Trem::run(){
                      x -= deslocamento;
                 }
 
+
                 //Região 2
                 else if (x == 440 && y == 210){
                     //Entrar na região 2
                     emit entrar_em_regiao(ID, 2);
+
                 } else if (x == 350 && y == 230){
                     //Sair da região 2
                     emit sair_de_regiao(2);
                     x -= deslocamento;
                 }
 
-                //Região 5
-                else if (x == 370 && y == 230){
-                    //Entrar na região 5
-                    emit entrar_em_regiao(ID, 5);
-                } else if (x == 350 && y == 230){
-                    //Sair da região 5
-                    emit sair_de_regiao(5);
-                    x -= deslocamento;
-                }
 
                 //Região 1
                 else if (x == 370 && y == 230){
                     //Entrar na região 1
                     emit entrar_em_regiao(ID, 1);
+
                 } else if (x == 260 && y == 230){
                     //Sair da região 1
                     emit sair_de_regiao(1);
                     y -= deslocamento;
                 }
+
 
                 //Zona livre do trem 1
                 else if (x == 260 && y > 110){
@@ -84,13 +85,6 @@ void Trem::run(){
                 } else if(x < 420 && y == 110){
                     //Segunda lateral
                     x += deslocamento;
-                } else if(x == 430 && y == 110){
-                    x += deslocamento;
-                } else if(x == 440 && y == 110){
-                    printf("VIRA BB");
-                    y += deslocamento;
-                } else if(x == 440 && y > 110){
-                    y += deslocamento;
                 }
 
 
@@ -100,7 +94,7 @@ void Trem::run(){
 
             case 2:
                 //Trem 2
-                // O Trem 2 usa as regiões: 4, 6, 3, 0.
+                // O Trem 2 usa as regiões: 4, 3, 0.
 
                 //Região 4
                 if (x == 610 && y == 210){
@@ -109,16 +103,6 @@ void Trem::run(){
                 } else if (x == 530 && y == 230){
                     //Sair da região 4
                     emit sair_de_regiao(4);
-                     x -= deslocamento;
-                }
-
-                //Região 6
-                else if (x == 550 && y == 230){
-                    //Entrar na região 6
-                    emit entrar_em_regiao(ID, 6);
-                } else if (x == 530 && y == 230){
-                    //Sair da região 6
-                    emit sair_de_regiao(6);
                      x -= deslocamento;
                 }
 
@@ -142,8 +126,8 @@ void Trem::run(){
                     x += deslocamento;
                 }
 
-                else if ((x > 440 && y == 110) && (x < 610 && y == 110)){
-                    //Zona livre do trem 2
+                //Zona livre do trem 2
+                else if ((x > 440 && y == 110) && (x < 610 && y == 110)){    
                     x += deslocamento;
                 } else if(x == 610 && y > 110){
                     y += deslocamento;
@@ -160,6 +144,7 @@ void Trem::run(){
                 //Trem 3
                 // O Trem 3 usa as regiões: 1, 5.
 
+                //Região 1
                 if (x == 240 && y == 230){
                     //Entrar na região 1
                     emit entrar_em_regiao(ID, 1);
@@ -171,6 +156,7 @@ void Trem::run(){
                      y += deslocamento;
                 }
 
+                //Região 5
                 else if (x == 330 && y == 230){
                     //Entrar na região 5
                     emit entrar_em_regiao(ID, 5);
@@ -197,54 +183,64 @@ void Trem::run(){
 
             case 4:
                 //Trem 4
-                // O Trem 4 usa as regiões: 5, 2, 0, 3, 6.
+                // O Trem 4 usa as regiões: 5, 2, 3, 6.
 
+                //Região 5
                 if (x == 370 && y == 350){
                     //Entrar na região 5
                     emit entrar_em_regiao(ID, 5);
+
+                } else if (x > 350 && y == 350){
+                    x -= deslocamento;
+
+                } else if (x == 350 && y > 350){
+                    y -= deslocamento;
+
                 } else if (x == 350 && y == 230){
                     //Sair da região 5
                     emit sair_de_regiao(5);
                     x += deslocamento;
                 }
 
-                else if (x == 350 && y == 250){
-                    //Entrar na região 2
-                    emit entrar_em_regiao(ID, 2);
-                } else if (x == 440 && y == 230){
-                    //Sair da região 2
-                    emit sair_de_regiao(2);
-                    x += deslocamento;
-                }
+//                //Região 2
+//                else if (x == 350 && y == 250){
+//                    //Entrar na região 2
+//                    emit entrar_em_regiao(ID, 2);
+//                } else if (x == 440 && y == 230){
+//                    //Sair da região 2
+//                    emit sair_de_regiao(2);
+//                    x += deslocamento;
+//                }
 
 
-                else if (x == 420 && y == 230){
-                    //Entrar na região 0 e 3
-                    emit entrar_em_regiao(ID, 0);
-                    emit entrar_em_regiao(ID, 3);
+//                //Região 3
+//                else if (x == 420 && y == 230){
+//                    //Entrar na região 3
+//                    emit entrar_em_regiao(ID, 3);
 
-                } else if (x == 440 && y == 230){
-                    //Sair da região 0
-                    emit sair_de_regiao(0);
-                    x += deslocamento;
+//                } else if (x == 440 && y == 230){
+//                    //Sair da região 0
+//                    emit sair_de_regiao(0);
+//                    x += deslocamento;
 
-                } else if (x == 530 && y == 230){
-                    //Sair da região 3
-                    emit sair_de_regiao(3);
-                    y -= deslocamento;
-                }
+//                } else if (x == 530 && y == 230){
+//                    //Sair da região 3
+//                    emit sair_de_regiao(3);
+//                    y -= deslocamento;
+//                }
 
 
-                else if (x == 510 && y == 230){
-                    //Entrar na região 6
-                    emit entrar_em_regiao(ID, 6);
-                } else if (x == 530 && y == 250){
-                    //Sair da região 6
-                    emit sair_de_regiao(6);
-                     x -= deslocamento;
-                }
-                else
-                    x -= deslocamento;
+//                //Região 6
+//                else if (x == 510 && y == 230){
+//                    //Entrar na região 6
+//                    emit entrar_em_regiao(ID, 6);
+//                } else if (x == 530 && y == 250){
+//                    //Sair da região 6
+//                    emit sair_de_regiao(6);
+//                     x -= deslocamento;
+//                }
+//                else
+//                    x -= deslocamento;
 
                 emit updateGUI(ID, x, y);    //Emite um sinal
                 break;
@@ -254,6 +250,7 @@ void Trem::run(){
                 //Trem 5
                 // O Trem 5 usa as regiões: 6, 4.
 
+                //Região 6
                 if (x == 550 && y == 350){
                     //Entrar na região 6
                     emit entrar_em_regiao(ID, 6);
@@ -265,17 +262,18 @@ void Trem::run(){
                      x += deslocamento;
                 }
 
+                //Região 4
                 else if (x == 530 && y == 250){
                     //Entrar na região 4
                     emit entrar_em_regiao(ID, 4);
                 }
-
                 else if (x == 610 && y == 230){
                     //Sair da região 4
                     emit sair_de_regiao(4);
                     x += deslocamento;
                 }
 
+                //Zona Livre
                 else if (x < 710 && y == 230){
                     //Zona livre do trem 4
                     x += deslocamento;
